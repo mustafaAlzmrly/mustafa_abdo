307bcc1b0124b7696e15753c33842661442c6131
abdulrahman <abdo.bello2000@gmail.com>
2024-02-02 21:52:49 +0200

    تعديل تسجيل الدخول واضافة الصلحيات

---
+++ AMS_PRO_MAX/AMS_PRO_MAX/UserP/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS_PRO_MAX.UserP
+{
+    using System;
+    using System.Collections.Generic;
+    public class Users
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public bool State { get; set; }
+        public int RoleID { get; set; }
+  
+    }
+}
