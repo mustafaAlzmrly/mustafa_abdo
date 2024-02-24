47909058c21fb2aa982a5fe9ad0825735217a1f5
AbdoEhneesh <abdo.bello2000@gmail.com>
2024-02-24 20:04:02 +0200

    اختبار تسجيل الدخول

---
+++ AMS_PRO_MAX/AMS_PRO_MAX.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AMS_PRO_MAX.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AMS_PRO_MAX.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd4e33a5-862b-4000-b1c7-7228b7736910")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AMS_PRO_MAX/AMS_PRO_MAX.Test/UserTest/loginTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AMS_PRO_MAX.Test.loginTest
+{
+    [TestClass]
+    public class loginTest
+    {
+        [TestMethod]
+        public void CheckLogin_ValidCredentials_ReturnsTrue()
+        {
+            // Arrange
+            var login = new Login(); 
+            login.txt_username.Text = "valid_username";
+            login.txt_pass.Text = "valid_password";
+
+            // Act
+            var result = login.Checklogin();
+
+            // Assert
+            Assert.IsTrue(result);
+            
+        }
+
+        [TestMethod]
+        public void CheckLogin_EmptyFields_ReturnsFalse()
+        {
+            // Arrange
+            var login = new Login(); 
+            login.txt_username.Text = "";
+            login.txt_pass.Text = "";
+
+            // Act
+            var result = login.Checklogin();
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
